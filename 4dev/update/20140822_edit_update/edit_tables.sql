dd4dc12ed43ac0679acdc83f4ff9c3f41b8b3f72
Clemens Schwaighofer <clemens.schwaighofer@e-graphics.com>
2014-09-01 15:13:25 +0900

    Update login to use two last error vars
    First error date for the first error occured, last one for the last
    error occured

--- 4dev/update/20140822_edit_update/edit_tables.sql
+++ 4dev/update/20140822_edit_update/edit_tables.sql
@@ -4,7 +4,8 @@
 -- count login errors
 ALTER TABLE edit_user ADD login_error_count INT DEFAULT 0;
 -- last login error date
-ALTER TABLE edit_user ADD login_error_date TIMESTAMP WITHOUT TIME ZONE;
+ALTER TABLE edit_user ADD login_error_date_last TIMESTAMP WITHOUT TIME ZONE;
+ALTER TABLE edit_user ADD login_error_date_first TIMESTAMP WITHOUT TIME ZONE;
 -- if this is set to true, this user gets locked after max login errors are reached
 ALTER TABLE edit_user ADD strict SMALLINT DEFAULT 0;
 ALTER TABLE edit_user ADD locked SMALLINT DEFAULT 0;
