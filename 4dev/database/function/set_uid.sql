719d82d4ee08d80badd25fd49ced457668fcfcef
Clemens Schwaighofer <clemens.schwaighofer@egplusww.com>
2020-01-28 11:07:12 +0900

    Update missing order settings in database import order file

--- 4dev/database/function/set_uid.sql
+++ 4dev/database/function/set_uid.sql
@@ -2,7 +2,7 @@
 
 CREATE OR REPLACE FUNCTION set_uid() RETURNS TRIGGER AS '
 	DECLARE
-		random_length INT = 12; -- that should be long enough
+		random_length INT = 32; -- that should be long enough
 	BEGIN
 		IF TG_OP = ''INSERT'' THEN
 			NEW.uid := random_string(random_length);
--- 4dev/database/function/update_function.sql
+++ 4dev/database/function/update_function.sql
@@ -1,14 +1,16 @@
 -- adds the created or updated date tags
 
-CREATE OR REPLACE FUNCTION set_generic() RETURNS TRIGGER AS '
-	BEGIN
-		IF TG_OP = ''INSERT'' THEN
-			NEW.date_created := clock_timestamp();
-			NEW.user_created := current_user;
-		ELSIF TG_OP = ''UPDATE'' THEN
-			NEW.date_updated := clock_timestamp();
-			NEW.user_updated := current_user;
-		END IF;
-		RETURN NEW;
-	END;
-' LANGUAGE 'plpgsql';
+-- OLD, DEPRECATED, use set_generic.sql
+
+-- CREATE OR REPLACE FUNCTION set_generic() RETURNS TRIGGER AS '
+-- 	BEGIN
+-- 		IF TG_OP = ''INSERT'' THEN
+-- 			NEW.date_created := clock_timestamp();
+-- 			NEW.user_created := current_user;
+-- 		ELSIF TG_OP = ''UPDATE'' THEN
+-- 			NEW.date_updated := clock_timestamp();
+-- 			NEW.user_updated := current_user;
+-- 		END IF;
+-- 		RETURN NEW;
+-- 	END;
+-- ' LANGUAGE 'plpgsql';
