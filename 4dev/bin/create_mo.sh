388b90913a9272b834d6cdd19178737e21d6d6d9
Clemens Schwaighofer <clemens.schwaighofer@egplusww.com>
2022-04-25 13:58:43 +0900

    translation creation script

---
+++ 4dev/bin/create_mo.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+base_folder='/var/www/html/developers/clemens/core_data/php_libraries/trunk/www/';
+
+# locale gettext po to mo translator master
+for file in $(ls -1 ${base_folder}../4dev/locale/*.po); do
+	file=$(basename $file .po);
+	echo "Translate language ${file}";
+	locale=$(echo "${file}" | cut -d "-" -f 1);
+	domain=$(echo "${file}" | cut -d "-" -f 2);
+	msgfmt -o ${base_folder}/includes/locale/${locale}/LC_MESSAGES/${domain}.mo ${base_folder}../4dev/locale/${locale}-${domain}.po;
+done;
+
+# __END__
