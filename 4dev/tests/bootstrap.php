a3bf38f6cf72ccf3ad455e615626588563a899e7
Clemens Schwaighofer <clemens.schwaighofer@egplusww.com>
2023-10-04 14:35:53 +0900

    Update bootstrap file for phpunit to work with different folder layouts
    we have the autoloader in different base folders for some dev work

--- 4dev/tests/bootstrap.php
+++ 4dev/tests/bootstrap.php
@@ -1,5 +1,15 @@
 <?php
 
-require dirname(__DIR__) . '../../www/vendor/autoload.php';
+$set = 0;
+foreach (['/../..', '/..', '/../../www', '/../www'] as $src) {
+	if (is_file(dirname(__DIR__) . $src . '/vendor/autoload.php')) {
+		require dirname(__DIR__) . $src . '/vendor/autoload.php';
+		$set = 1;
+		break;
+	}
+}
+if (!$set) {
+	die("Cannot find /vendor/autoload.php in reference to: " . dirname(__DIR__));
+}
 
 // __END__
