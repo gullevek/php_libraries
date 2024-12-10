46e44c19bfea238243530607d317bdd7bb112daf
Clemens Schwaighofer <clemens.schwaighofer@egplusww.com>
2024-12-10 14:44:00 +0900

    edit log table column order update

--- 4dev/database/table/edit_log.sql
+++ 4dev/database/table/edit_log.sql
@@ -12,15 +12,24 @@ CREATE TABLE edit_log (
     FOREIGN KEY (euid) REFERENCES edit_user (edit_user_id) MATCH FULL ON UPDATE CASCADE ON DELETE SET NULL,
     ecuid VARCHAR,
     ecuuid UUID, -- this is the one we want to use, full UUIDv4 from the edit user table
-    username VARCHAR,
-    password VARCHAR,
+    -- date_created equal, but can be overridden
     event_date TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    ip VARCHAR, -- just the REMOTE_IP, full set see ip_address
+    -- session ID if set
+    session_id VARCHAR,
+    -- username
+    username VARCHAR,
+    -- DEPRECATED [password]
+    password VARCHAR,
     ip_address JSONB, -- REMOTE_IP and all other IPs (X_FORWARD, etc) as JSON block
+    -- DEPRECATED [ip]
+    ip VARCHAR, -- just the REMOTE_IP, full set see ip_address
+    -- string blocks, general
     error TEXT,
     event TEXT,
+    -- bytea or string type storage of any data
     data_binary BYTEA,
     data TEXT,
+    -- set page name only
     page VARCHAR,
     -- various info data sets
     user_agent VARCHAR,
@@ -31,13 +40,13 @@ CREATE TABLE edit_log (
     server_name VARCHAR,
     http_host VARCHAR,
     http_data JSONB,
+    -- DEPRECATED [http*]
     http_accept VARCHAR, -- in http_data
     http_accept_charset VARCHAR, -- in http_data
     http_accept_encoding VARCHAR, -- in http_data
-    -- session ID if set
-    session_id VARCHAR.
     -- any action var, -> same set in action_data as JSON
     action_data JSONB,
+    -- DEPRECATED [action*]
     action VARCHAR, -- in action_data
     action_id VARCHAR, -- in action_data
     action_sub_id VARCHAR, -- in action_data
