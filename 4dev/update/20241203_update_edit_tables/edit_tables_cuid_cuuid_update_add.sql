540269e61ffd180e4cabb47baf6e155a0e1f2798
Clemens Schwaighofer <clemens.schwaighofer@egplusww.com>
2024-12-12 19:04:21 +0900

    Fix update script for now to clock_timestamp

--- 4dev/update/20241203_update_edit_tables/edit_tables_cuid_cuuid_update_add.sql
+++ 4dev/update/20241203_update_edit_tables/edit_tables_cuid_cuuid_update_add.sql
@@ -22,11 +22,11 @@ DECLARE
     random_length INT = 25; -- that should be long enough
 BEGIN
     IF TG_OP = 'INSERT' THEN
-        NEW.date_created := 'now';
+        NEW.date_created := clock_timestamp();
         NEW.cuid := random_string(random_length);
         NEW.cuuid := gen_random_uuid();
     ELSIF TG_OP = 'UPDATE' THEN
-        NEW.date_updated := 'now';
+        NEW.date_updated := clock_timestamp();
     END IF;
     RETURN NEW;
 END;
