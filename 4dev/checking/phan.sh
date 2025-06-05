ad7b59e26ac2598e87a0cfeae73f273304edbd8d
Clemens Schwaighofer <clemens.schwaighofer@egplusww.com>
2025-06-05 18:02:03 +0900

    phan check swich from phive to composer package

--- 4dev/checking/phan.sh
+++ 4dev/checking/phan.sh
@@ -1,5 +1,6 @@
 base=$(pwd)"/";
 # must be run in ${base}
 cd $base || exit;
-PHAN_DISABLE_XDEBUG_WARN=1;${base}tools/phan --progress-bar -C --analyze-twice
+#PHAN_DISABLE_XDEBUG_WARN=1;${base}tools/phan --progress-bar -C --analyze-twice
+PHAN_DISABLE_XDEBUG_WARN=1;${base}vendor/bin/phan --progress-bar -C --analyze-twice
 cd ~ || exit;
